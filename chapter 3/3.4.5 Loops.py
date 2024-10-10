03fea26ea9e36fb005544b182c0cea8387176978
Anil Kumar <121234214+akumar2312@users.noreply.github.com>
2024-10-10 15:32:14 +0530

    Create 3.4.5 Loops.py

---
+++ chapter 3/3.4.5 Loops.py
@@ -0,0 +1,11 @@
+# Using a for loop
+print("Using a for loop:")
+for i in range(1, 11):
+    print(i)
+
+# Using a while loop
+print("\nUsing a while loop:")
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
