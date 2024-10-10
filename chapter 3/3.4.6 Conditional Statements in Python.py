77cd4dec7e95c5667527a230703cbcd3c3576b65
Anil Kumar <121234214+akumar2312@users.noreply.github.com>
2024-10-10 15:33:00 +0530

    Create 3.4.6 Conditional Statements in Python.py

---
+++ chapter 3/3.4.6 Conditional Statements in Python.py
@@ -0,0 +1,10 @@
+# Take input from the user
+num = float(input("Enter a number: "))
+
+# Use conditional statements to check the number
+if num > 0:
+    print("The number is positive")
+elif num == 0:
+    print("The number is zero")
+else:
+    print("The number is negative")
